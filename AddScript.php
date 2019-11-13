b943815cc4cb515a18461d0c80fde35c9f67f0d0
juek <jk@grafikrausz.at>
2019-11-13 02:25:12 +0100

    ver. 1.1.0
    adds some features

--- AddScript.php
+++ AddScript.php
@@ -6,13 +6,22 @@ Author: Florin-Cătălin Tofan
 Copyright (c) 2019 Florin-Cătălin Tofan, MIT License (MIT)
 Date: 01.10.2019
 Version 1.0
-  This is my first plugin for Typesetter CMS (https://github.com/Typesetter/Typesetter). As the documentation was scarce, I used ppeterka's EasyMark plugin as the main source of information (https://github.com/ppeterka/easymark), MIT license. 
-  The AddScript plugin can be used to enter source code directly into the page. CKEditor3.4, the basic editor of Typesetter5.1 does not allow this, but it can be configured to allow editing, with certain limitations.
-  However, the AddScript plugin makes it much easier to insert any code without modifying it in any way. For example, you can easily insert any Facebook script.
+
+  This is my first plugin for Typesetter CMS (https://github.com/Typesetter/Typesetter). 
+  As the documentation was scarce, I used ppeterka's EasyMark plugin as the main source 
+  of information (https://github.com/ppeterka/easymark), MIT license. 
+  The AddScript plugin can be used to enter source code directly into the page. 
+  CKEditor, the basic editor of Typesetter5.1 does not allow this, but it can be configured 
+  to allow editing, with certain limitations. However, the AddScript plugin makes it much 
+  easier to insert any code without modifying it in any way. 
+  For example, you can easily insert any Facebook script.
   After installing the plugin it can be easily inserted into the page, similar to other objects.
   The benefit of such a plugin is considerable.
-  The disadvantage is that it requires great attention, as a malfunctioning script can ruin the entire page. So it is a useful script especially for those who have programming knowledge. Use it at your own risk!
+  The disadvantage is that it requires great attention, as a malfunctioning script can ruin the entire page. 
+  So it is a useful script especially for those who have programming knowledge. 
+  Use it at your own risk!
   zenodo
+
 ######################################################################
 */
 
@@ -23,56 +32,156 @@ class AddScript
 {
 	public static $sectionType = 'addscript_section';
 
-	function SectionTypes( $section_types) {
+
+	static function GetHead() {
+		\gp\tool\Plugins::css('css/user.css');
+		if( !\gp\tool::LoggedIn() ){
+			return;
+		}
+		\gp\tool\Plugins::css('css/admin.css', false);
+		// \gp\tool\Plugins::js('js/AddScript_admin.js', false);
+	}
+
+
+	static function SectionTypes($section_types) {
 		$section_types[self::$sectionType] = array();
 		$section_types[self::$sectionType]['label'] = 'AddScript';
 		return $section_types;
 	}
 
-	function SectionToContent($section_data) {
-		global $addonPathCode,$addonPathData;
+
+	static function NewSections($links){
+		global $addonRelativeCode;
+		foreach( $links as $key => $link ){
+			$match = is_array($link[0]) ? implode('-', $link[0]) : $link[0];
+			if( $match ==  self::$sectionType ){
+				$links[$key][1] = $addonRelativeCode . '/icons/ui-icon.png'; 
+				break;
+			}
+		}
+		return $links;
+	}
+
+
+	static function SectionToContent($section_data) {
+		global $page;
+
+		if( empty($section_data['script']) ){
+			return $section_data;
+		}
+
+		switch( $section_data['script_type'] ){
+			case 'jQuery':
+				$page->jQueryCode .= "\n/* AddScript section START */\n" 
+					. $section_data['script'] 
+					. "\n/* AddScript section END */\n";
+				break;
+
+			case 'js':
+				$page->head_script .= "\n/* AddScript section START */\n" 
+					. $section_data['script']
+					. "\n/* AddScript section END */\n";
+				break;
+
+			case 'url':
+				$page->head .= "\n<!-- AddScript section START -->\n" 
+					. '<script src="' . $section_data['script'] . '"></script>' 
+					. "\n<!-- AddScript section END -->\n"; 
+				break;
+
+			case 'raw':
+				$section_data['content'] = "<!-- AddScript Section Start -->\n" 
+					. $section_data['script'] 
+					. "\n<!-- AddScript section END -->";
+				break;
+
+		}
+
 		return $section_data;
 	}
-	
-	function DefaultContent($default_content,$type) {
-		if( $type == self::$sectionType ) {
-			$section = array();
-			$section['content'] = "Adauga script";
-			return $section;
+
+
+	static function ToggleClass($section_data, $class, $toggle=true) {
+		if( empty($section_data['attributes']['class']) ){
+			if( $toggle ){
+				$section_data['attributes'] = array( 'class' => $class );
+				return $section_data;
+			}
 		}
-		return $default_content;
+		$classes = explode(' ', $section_data['attributes']['class']);
+		$classes = array_map('trim', $classes);
+		if( $toggle && !in_array($class, $classes) ){
+			$classes[] = $class;
+		}
+		if( !$toggle && in_array($class, $classes) ){
+			$key = array_search($class, $classes);
+			unset($classes[$key]);
+		}
+		$section_data['attributes']['class'] = implode(' ', $classes);
+		return $section_data;
 	}
 
-	
-	function SaveSection($return,$section,$type) {
-		if( $type != self::$sectionType ) {
-		  return $return;
+
+	static function DefaultContent($default_content, $type) {
+		if( $type != self::$sectionType ){
+			return $default_content;
 		}
+		$section = array();
+		$section['content']			= '<!-- AddScript Section -->';
+		$section['script_type']		= 'js'; // 'js' | 'jQuery' | 'url' | 'raw'
+		$section['script']			= '';
+		return $section;
+	}
+
+
+	static function SaveSection($return, $section, $type) {
 		global $page;
-		$content =& $_POST['gpcontent'];
-		$page->file_sections[$section]['content'] = $content;
+		if( $type != self::$sectionType ){
+			return $return;
+		}
+
+		$section_data 					= $page->file_sections[$section];
+		$section_data['script_type']	= trim($_POST['script_type']);
+		$section_data['script']			= trim($_POST['script']);
+
+		$section_data = self::ToggleClass($section_data, 'addscript-section-empty', false);
+		$section_data = self::ToggleClass($section_data, 'addscript-section-raw-output', false);
+		if( empty($script) ){
+			$section_data['content'] = '<!-- EMPTY AddScript Section -->';
+			$section_data = self::ToggleClass($section_data, 'addscript-section-empty', true);
+		}elseif( $section_data['script_type'] == 'raw' ){
+			$section_data = self::ToggleClass($section_data, 'addscript-section-raw-output', true);
+		}else{
+			$section_data['content'] = '<!-- AddScript Section -->';
+		}
+
+
+		$page->file_sections[$section] = $section_data;
+
 		return true;
 	}
-	
-	
-	function GenerateContent_Admin() {
-		global $addonFolderName, $page;
-		static $done = false;
-		if ($done || !common::LoggedIn()) { return; }
-		$done = true;
-	}
-	
-	
-	function InlineEdit_Scripts($scripts,$type) {
-		global $addonPathCode;
-		
-		if( $type != self::$sectionType ) {
+
+
+	static function InlineEdit_Scripts($scripts, $type) {
+		global $addonRelativeCode, $addonFolderName;;
+
+		if( $type != self::$sectionType ){
 		  return $scripts;
 		}
-		
-		$scripts[] = $addonPathCode.'/js/edit.js';
+
+		$addonBasePath = (strpos($addonRelativeCode, 'addons/') > 0) 
+			? '/addons/' . $addonFolderName 
+			: '/data/_addoncode/' . $addonFolderName;
+		echo "\n" . 'AddScript_base = "' . $addonBasePath . '";' . "\n";
+
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/lib/codemirror.min.js';
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/mode/xml/xml.min.js';
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/mode/javascript/javascript.min.js';
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/mode/css/css.min.js';
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/mode/htmlmixed/htmlmixed.min.js';
+		$scripts[] = $addonRelativeCode . '/thirdparty/codemirror/addon/display/placeholder.min.js';
+		$scripts[] = $addonRelativeCode . '/js/edit.js';
 		return $scripts; 
 	}
+
 }
-
-
--- js/edit.js
+++ js/edit.js
@@ -1,34 +1,160 @@
-/**
- *  Copied and changed from "Execute PHP" plugin
- */
-function gp_init_inline_edit(area_id,section_object){
+function gp_init_inline_edit(area_id, section_object){
 	var textarea, cache;
 
-	loaded();
+	$gp.LoadStyle(AddScript_base + '/thirdparty/codemirror/lib/codemirror.min.css', false);
+	$gp.LoadStyle(AddScript_base + '/css/edit.css', false);
 	gp_editing.editor_tools();
 	var edit_div = gp_editing.get_edit_area(area_id);
 
 	//set up textarea
-	textarea = $('<textarea style="width:100%;margin:0;border:0 none;" />')
-				.val(section_object.content);
-	edit_div.html(textarea);
+	$textarea = $('<textarea placeholder="Add your custom code here..."></textarea>').val(section_object.script);
+	edit_div.html($textarea);
+
 
 	gp_editor = {
-		save_path: gp_editing.get_path(area_id),
 
-		destroy:function(){},
-		checkDirty:function(){
-			if( cache == textarea.val() ){
-				return false;
+		init : function(){
+			gp_editor.createEditorUi();
+			gp_editor.createCodeMirror(gp_editor.script_type);
+			gp_editor.resetDirty();
+			loaded();
+		},
+
+
+		ui : {},
+
+
+		script_type : section_object.script_type, 
+
+
+		CanAutoSave : function(){ 
+			return false;  // prevent saving invalid script fragments
+		},
+
+
+		sleep : function(){ 
+			edit_div.find('.CodeMirror').slideUp(); 
+			edit_div.find('.addscript-error-msg').hide(); 
+		},
+
+
+		wake : function(){ 
+			edit_div.find('.CodeMirror').slideDown(); 
+			edit_div.find('.addscript-error-msg').show(); 
+		},
+
+
+		save_path : gp_editing.get_path(area_id),
+
+
+		destroy : function(){},
+
+
+		checkDirty : function(){
+			return cache != gp_editor.gp_saveData(true);
+		},
+
+
+		gp_saveData : function(get_cache){
+			var script = '';
+			if( typeof(get_cache) != 'boolean' ){
+				gp_editor.checkJs();
 			}
-			return true;
+			return 'script=' 
+				+ encodeURIComponent(gp_editor.codeMirror.getValue())
+				+ '&script_type=' + gp_editor.script_type;
 		},
-		gp_saveData:function(){
-			return 'gpcontent='+encodeURIComponent(textarea.val());
+
+
+
+		resetDirty : function(){
+			cache = gp_editor.gp_saveData(true);
 		},
-		resetDirty:function(){
-			cache = textarea.val();
+
+
+		updateElement : function(){},
+
+
+		checkJs : function(){
+			edit_div
+				.removeClass('addscript-has-errors')
+				.find('.addscript-error-msg')
+					.remove();
+			if( gp_editor.script_type == 'url' || gp_editor.script_type == 'raw' ){
+				return true;
+			}
+			try{
+				eval(gp_editor.codeMirror.getValue()); // yep, it's the evil eval
+			}catch(e){
+				if( e instanceof SyntaxError ){
+					edit_div
+						.addClass('addscript-has-errors')
+						.append('<div class="addscript-error-msg">'
+						+	'<a class="addscript-error-msg-btn" onclick="gp_editor.checkJs()">Check again</a>'
+						+	'<span class="addscript-error-msg-text"><i class="fa fa-exclamation-circle"></i> '
+						+	'Syntax Error: ' + e.message + '</span>'
+						+ '</div>');
+						return false;
+				}else{
+					return true;
+				}
+			}
 		},
-		updateElement:function(){}
-	}
+
+
+		codeMirror : null,
+
+
+		createCodeMirror : function(script_type){
+			if( gp_editor.codeMirror ){
+				gp_editor.codeMirror.toTextArea(); // destroy current
+			}
+			var codeMirrorConfig = { lineWrapping : true };
+			switch(script_type){
+				case 'js':
+				case 'jQuery':
+					codeMirrorConfig.mode = 'text/javascript';
+					break;
+				case 'raw':
+					codeMirrorConfig.mode = 'text/html';
+					break;
+			}
+			gp_editor.codeMirror = CodeMirror.fromTextArea($textarea.get(0), codeMirrorConfig);
+			gp_editor.checkJs();
+		},
+
+
+		createEditorUi : function(){
+			gp_editor.ui.changeType = $('<div class="addscript-change-type">'
+				+ '<label><input type="radio" name="changetype" value="js" />JavaScript</label>'
+				+ '<label><input type="radio" name="changetype" value="jQuery" />jQuery</label>'
+				+ '<label><input type="radio" name="changetype" value="url" />Script URL</label>'
+				+ '<label><input type="radio" name="changetype" value="raw" />Raw Output (in place)</label>'
+				+ '</div>')
+				.appendTo('#ckeditor_controls');
+
+			gp_editor.ui.changeType
+				.find('input[value="' + gp_editor.script_type + '"]')
+					.prop('checked', true)
+					.closest('label').addClass('is-checked');
+
+			gp_editor.ui.changeType
+				.find('input[type="radio"]')
+				.on('change', function(){
+					gp_editor.script_type = $(this).val();
+					gp_editor.createCodeMirror( gp_editor.script_type );
+					$(this).closest('label').addClass('is-checked')
+						.siblings().removeClass('is-checked');
+				});
+		}
+
+	};
+
+
+	/* ############ */
+	/* ### INIT ### */
+	/* ############ */
+
+	gp_editor.init();
+
 }
